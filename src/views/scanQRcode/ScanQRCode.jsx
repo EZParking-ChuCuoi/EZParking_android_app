191d7b9cacff16134a2742878625b4ebd5332e73
dennis <laplepnv23@gmail.com>
2023-03-10 13:55:42 +0700

    Updating ui for scanner QR code

--- src/views/scanQRcode/ScanQRCode.jsx
+++ src/views/scanQRcode/ScanQRCode.jsx
@@ -1,11 +1,17 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import EZContainer from '../../components/core/EZContainer';
 import EZText from '../../components/core/EZText';
-import {EZButton, EZButtonBack} from '../../components/core/EZButton';
+import {
+  EZButton,
+  EZButtonBack,
+  EZButtonText,
+} from '../../components/core/EZButton';
 import {COLORS, EZStatusBar} from '../../assets/styles/styles';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
 import {BarcodeFormat, useScanBarcodes} from 'vision-camera-code-scanner';
+import EZBgTopRounded from '../../components/core/EZBgTopRounded';
+import Lottie from 'lottie-react-native';
 
 const ScanQRCode = () => {
   const [hasPermission, setHasPermission] = useState(false);
@@ -21,26 +27,45 @@ const ScanQRCode = () => {
       setHasPermission(status === 'authorized');
     })();
   }, []);
+  useEffect(() => {}, [barcodes]);
   return (
     device != null &&
     hasPermission && (
-      <>
-        <Camera
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={true}
-          frameProcessor={frameProcessor}
-          frameProcessorFps={5}
-        />
-        <View style={styles.content}>
-          <EZText>Scan result:</EZText>
-          {barcodes.map((barcode, idx) => (
-            <EZText key={idx} style={styles.barcodeTextURL}>
-              {barcode.displayValue}
-            </EZText>
-          ))}
+      <EZContainer
+        bgEZStatusBar={COLORS.tertiary}
+        styleEZContainer={styles.container}>
+        <EZBgTopRounded styleEZBgTopRounded={styles.bgTop} height={120}>
+          <EZText size="large" bold>
+            Scan QR code
+          </EZText>
+        </EZBgTopRounded>
+        <View style={styles.mainContent}>
+          <View style={styles.cameraWrapper}>
+            <Camera
+              style={styles.camera}
+              device={device}
+              isActive={true}
+              frameProcessor={frameProcessor}
+              frameProcessorFps={5}
+              enableZoomGesture={true}
+            />
+            <Lottie
+              source={require('../../assets/images/scan.json')}
+              autoPlay
+              loop
+              style={styles.imageScan}
+            />
+          </View>
+          <View style={styles.content}>
+            <EZText>Scan result:</EZText>
+            {barcodes.map((barcode, idx) => (
+              <EZText key={idx} style={styles.barcodeTextURL}>
+                {barcode.displayValue}
+              </EZText>
+            ))}
+          </View>
         </View>
-      </>
+      </EZContainer>
     )
   );
 };
@@ -48,7 +73,44 @@ const ScanQRCode = () => {
 export default ScanQRCode;
 
 const styles = StyleSheet.create({
+  camera: {
+    position: 'relative',
+    width: Dimensions.get('screen').width - 25,
+    height: Dimensions.get('screen').width - 25,
+  },
+  cameraWrapper: {
+    width: Dimensions.get('screen').width - 20,
+    height: Dimensions.get('screen').width - 20,
+    borderWidth: 2,
+    borderColor: COLORS.strokeColor,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  container: {
+    alignItems: 'center',
+  },
+  bgTop: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainContent: {
+    width: '100%',
+    height: '100%',
+    paddingTop: 50,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  imageScan: {
+    width: Dimensions.get('screen').width,
+    height: Dimensions.get('screen').width,
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
   content: {
-    backgroundColor: COLORS.tertiary,
+    marginTop: 20,
+    width: '100%',
+    alignItems: 'center',
   },
 });
